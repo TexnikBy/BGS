236717836e0d8a2f2110de4f060bdac04ac43d58
Dubinin, Aleksey2 <26743723+TexnikBy@users.noreply.github.com>
2023-10-27 00:32:06 +0300

    added shared kernel

---
+++ server/src/common/BGS.SharedKernel/EntityBase.cs
@@ -0,0 +1,6 @@
+﻿namespace BGS.SharedKernel;
+
+public abstract class EntityBase
+{
+    public Guid Id { get; set; }
+}
---
+++ server/src/common/BGS.SharedKernel/IRepository.cs
@@ -0,0 +1,5 @@
+﻿using Ardalis.Specification;
+
+namespace BGS.SharedKernel;
+
+public interface IRepository<T> : IRepositoryBase<T> where T : EntityBase { }
