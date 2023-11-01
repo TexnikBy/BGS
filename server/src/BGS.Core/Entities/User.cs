886c7e37a151f180143c7dd898d26063db6b9490
Alina <adelheid0106@gmail.com>
2023-11-01 16:28:17 +0300

    Remove migration and Users entity

--- server/src/BGS.Core/Entities/User.cs
+++
@@ -1,8 +0,0 @@
-using BGS.SharedKernel;
-
-namespace BGS.ApplicationCore.Entities;
-
-public class User : BaseEntity
-{
-    public string Name { get; set; }
-}
--- server/src/BGS.Infrastructure/Data/ApplicationDbContext.cs
+++ server/src/BGS.Infrastructure/Data/ApplicationDbContext.cs
@@ -5,7 +5,6 @@ namespace BGS.Infrastructure.Data;
 
 public class ApplicationDbContext : DbContext
 {
-    public DbSet<User> Users { get; set; }
     public DbSet<Game> Games { get; set; }
     
     public ApplicationDbContext(DbContextOptions options)
