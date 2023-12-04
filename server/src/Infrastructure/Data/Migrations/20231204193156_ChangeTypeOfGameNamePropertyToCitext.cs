c89ee9b233337027bd4e2364e369cd1e39244e6e
Dubinin, Aleksey2 <26743723+TexnikBy@users.noreply.github.com>
2023-12-04 22:46:49 +0300

    added ChangeTypeOfGameNamePropertyToCitext migration

---
+++ server/src/Infrastructure/Data/Migrations/20231204193156_ChangeTypeOfGameNamePropertyToCitext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BGS.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTypeOfGameNamePropertyToCitext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "game",
+                type: "citext",
+                maxLength: 65,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(65)",
+                oldMaxLength: 65);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "game",
+                type: "character varying(65)",
+                maxLength: 65,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "citext",
+                oldMaxLength: 65);
+        }
+    }
+}
