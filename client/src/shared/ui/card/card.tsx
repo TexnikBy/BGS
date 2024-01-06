ba205a3d5df59f9770efa87596fe517db6355082
Dubinin, Aleksey2 <26743723+TexnikBy@users.noreply.github.com>
2024-01-06 16:55:18 +0300

    add card component

--- client/src/shared/ui/card/card.tsx
+++ client/src/shared/ui/card/card.tsx
@@ -3,12 +3,14 @@ import classNames from "classnames";
 
 interface Props extends PropsWithChildren {
     className?: string;
+    onClick?: () => void;
 }
 
 export const Card = (props: Props) => {
     return (
-        <div className={classNames("card", props.className)}>
+        <div className={classNames("card", props.className)}
+             onClick={props.onClick}>
             {props.children}
         </div>
     );
-}
+};
