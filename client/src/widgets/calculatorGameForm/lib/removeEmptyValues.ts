980d4fa90488b4db5d0c9bb40933f2401fc0708c
Dubinin, Aleksey2 <26743723+TexnikBy@users.noreply.github.com>
2024-02-04 01:58:49 +0300

    fix when value in array null

--- client/src/widgets/calculatorGameForm/lib/removeEmptyValues.ts
+++ client/src/widgets/calculatorGameForm/lib/removeEmptyValues.ts
@@ -4,10 +4,10 @@ export function removeEmptyValues(obj: any) {
     if (Array.isArray(obj)) {
         return obj
             .map(v => (v && typeof v === 'object') ? removeEmptyValues(v) : v)
-            .filter(v => !(v === ""));
+            .filter(v => (!(v === "") && !(v != null)));
     } else {
         return Object.entries(obj)
             .map(([k, v]) => [k, v && typeof v === 'object' ? removeEmptyValues(v) : v])
-            .reduce((a, [k, v]) => (v === "" ? a : (a[k]=v, a)), {});
+            .reduce((a, [k, v]) => (v == null && v === "" ? a : (a[k]=v, a)), {});
     }
 }
